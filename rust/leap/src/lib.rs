e5ace6c16c82f90331176f7f0097ff1597dc57d1
Jordan McAlpine <jordan@mcalpine.dev>
2023-04-02 00:16:07 -0400

    completed: rust leap

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,3 +1,33 @@
 pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {year} is a leap year")
+    if year % 400 == 0 {
+        return true;
+    }
+
+    if year % 100 == 0 {
+        return false;
+    }
+
+    if year % 4 == 0 {
+        return true;
+    }
+
+    return false;
+}
+
+pub fn is_leap_year_using_match1(year: u64) -> bool {
+    match (year % 400, year % 100, year % 4) {
+        (0, _, _) => true,
+        (_, 0, _) => false,
+        (_, _, 0) => true,
+        _ => false,
+    }
+}
+
+pub fn is_leap_year_using_match2(year: u64) -> bool {
+    match year {
+        x if x % 400 == 0 => true,
+        x if x % 100 == 0 => false,
+        x if x % 4 == 0 => true,
+        _ => false,
+    }
 }
